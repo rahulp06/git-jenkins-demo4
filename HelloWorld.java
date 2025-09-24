30ca60dcb93c5bf19b1318c2c345216ed3dd5bac
Rahul Pillem <yoursrahul1605@gmail.com>
2025-09-24 19:15:23 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld{
     public static void main(String args[]){
         int a=3,b=5;
         int sum=a+b;
-        System.out.println("Sum of"+a+"+"+b+"is"+sum);
+        System.out.println("Sum of "+a+" + "+b+" is "+sum);
     }
 }
+
 
